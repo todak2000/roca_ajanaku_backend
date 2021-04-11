d31b6d486018e738f75a9c2bf1e7e8b9470614dc
Daniel Olagunju <todak2000@gmail.com>
2021-04-11 19:46:24 +0100

    buggy

--- roca/settings.py
+++ roca/settings.py
@@ -42,6 +42,7 @@ EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Application definition
 
