404863e5fa1d73d3f3842eeb3f8b75b750eb1ed5
Daniel Olagunju <todak2000@gmail.com>
2021-04-12 19:50:11 +0100

    email bug

--- roca/settings.py
+++ roca/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from decouple import config
 # from corsheaders.defaults import default_headers
-# import django_heroku # remove in local machine
+import django_heroku # remove in local machine
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
