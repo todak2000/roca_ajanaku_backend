da4d491909a18defff50e588f6c2c8fc3780993e
Daniel Olagunju <todak2000@gmail.com>
2021-04-12 20:32:20 +0100

    email bug

--- roca/urls.py
+++ roca/urls.py
@@ -21,4 +21,7 @@ from django.conf.urls.static import static # new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('server/', include('server.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
