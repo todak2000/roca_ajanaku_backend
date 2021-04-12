07e0e5add6fb40653c4ca59d9dddc6545dd5da4d
Daniel Olagunju <todak2000@gmail.com>
2021-04-13 00:16:25 +0100

    bug fix

--- server/models.py
+++ server/models.py
@@ -7,8 +7,8 @@ class JoinUs(models.Model):
     class Meta:
         db_table = "Prospective Employees"
     name = models.CharField(max_length=30,verbose_name="Name",blank=True)
-    email = models.EmailField(max_length=90, unique=True,verbose_name="Email")
-    phone = models.CharField(max_length=15, unique=True, null=True, verbose_name="Telephone number")
+    email = models.EmailField(max_length=90, verbose_name="Email", blank=True)
+    phone = models.CharField(max_length=15, verbose_name="Telephone number",blank=True)
     document = models.FileField(upload_to='documents/')
     date_added = models.DateTimeField(default=timezone.now)
     def __str__(self):
