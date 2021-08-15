f938ef10a2a95700fec85875c4efbc259bdb9757
Daniel <todak2000@gmail.com>
2021-08-15 21:48:32 +0100

    fixing model

--- server/models.py
+++ server/models.py
@@ -19,12 +19,12 @@ class Quidroo(models.Model):
         db_table = "Prospective Users Data"
     name = models.CharField(max_length=100,verbose_name="Name",blank=True)
     email = models.EmailField(max_length=100, verbose_name="Email", blank=True)
-    phone = models.CharField(max_length=15, verbose_name="Telephone number",blank=True)
+    phone = models.CharField(max_length=1500, verbose_name="Telephone number",blank=True)
     company = models.CharField(max_length=2000, verbose_name="Company",blank=True)
 
     type_business = models.CharField(max_length=2000, verbose_name="Type of Business",blank=True)
     address = models.CharField(max_length=2000, verbose_name="Company Address",blank=True)
-    confirm_female_cofounder = models.CharField(max_length=15, verbose_name="Is there atleast one female cofounder/owner",blank=True)
+    confirm_female_cofounder = models.CharField(max_length=1500, verbose_name="Is there atleast one female cofounder/owner",blank=True)
 
     date_added = models.DateTimeField(default=timezone.now)
     def __str__(self):
