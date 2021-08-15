f366ff4b39d65a6906536f979bdec9668d322308
Daniel <todak2000@gmail.com>
2021-08-15 21:38:17 +0100

    fixng quidroo model bug

--- server/models.py
+++ server/models.py
@@ -17,13 +17,13 @@ class JoinUs(models.Model):
 class Quidroo(models.Model):
     class Meta:
         db_table = "Prospective Users Data"
-    name = models.CharField(max_length=30,verbose_name="Name",blank=True)
-    email = models.EmailField(max_length=90, verbose_name="Email", blank=True)
+    name = models.CharField(max_length=100,verbose_name="Name",blank=True)
+    email = models.EmailField(max_length=100, verbose_name="Email", blank=True)
     phone = models.CharField(max_length=15, verbose_name="Telephone number",blank=True)
-    company = models.CharField(max_length=15, verbose_name="Company",blank=True)
+    company = models.CharField(max_length=2000, verbose_name="Company",blank=True)
 
-    type_business = models.CharField(max_length=15, verbose_name="Type of Business",blank=True)
-    address = models.CharField(max_length=15, verbose_name="Company Address",blank=True)
+    type_business = models.CharField(max_length=2000, verbose_name="Type of Business",blank=True)
+    address = models.CharField(max_length=2000, verbose_name="Company Address",blank=True)
     confirm_female_cofounder = models.CharField(max_length=15, verbose_name="Is there atleast one female cofounder/owner",blank=True)
 
     date_added = models.DateTimeField(default=timezone.now)
